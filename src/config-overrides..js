d96a7510ffd57fbdbdbfe7058145b9fbfcf53340
leafyann <sgsnif@gmail.com>
2020-03-29 20:23:13 +0300

    add customize-cra adn react-app-rewired

---
+++ src/config-overrides..js
@@ -0,0 +1,10 @@
+/* 
+ * @file config-overrides.js
+ * @author Qi Wu(sgsnif@gmail.com)
+ * 基于customize和react-app-rewired的定制化配置文件
+*/
+
+//从customize-cra引入一些相关的方法
+const { override } = require('customize-cra')
+
+module.exports = override()
